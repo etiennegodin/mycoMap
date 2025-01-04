6b1b69af3075e6541599c07e302c43b1af0e82b7
Etienne <manateetiti@gmail.com>
2025-01-03 20:37:19 -0500

    fixed occurences, will need to split search and download function (max 300 search), added mycoQuebec.py to webscrape infos

---
+++ mycoQuebec.py
@@ -0,0 +1,12 @@
+
+
+# web scrape myco quebec to get infos
+
+#species name from list ( genre edible)
+
+
+#ecologie
+#Periodes
+
+
+#write to file to populate in Specie object
--- occurences.py
+++ occurences.py
@@ -4,50 +4,54 @@ import os
 from pygbif import occurrences
 
 
-def searchOccurences(specie, limit = 2, year = '2024'):
+def searchOccurences(specie, limit = 300):
 
     #quebec geomtry as WKT
     '''
     with open("data/geodata/test2.txt", 'r') as file:
         geometry = file.read()
     '''
-
-    
     decimal_longitude = '-79.3439314990000071,-63.9999979090000011'
     decimal_latitude = '45.0000682390000009, 50.0000022050000013'
-
-    occurences = occurrences.search(taxonKey = specie.key, limit= limit, hasCoordinate=True, hasGeospatialIssue = False, decimalLongitude= decimal_longitude, decimalLatitude=decimal_latitude)
-
-    #print(occurences)
-
-    #info kept from query
-    occurenceList = []
-    occurenceKey = []
-    occurenceLon = []
-    occurenceLat = []
-    occurenceId = []
-
-    print("Found {} available occurences for {} in Quebec, limiting to {}".format((occurences['count']), specie.name, limit))
-
-
-############broken writting info from occ object to pd ######################
-    #cycling through occurences to keep info
-    for index, o in enumerate(occurences["results"]):
-        occurenceKey.append(o["key"])
-        occurenceLon.append( o['decimalLongitude'])
-        occurenceLat.append(o["decimalLatitude"])
-        occurenceId.append(o["occurrenceID"])
-
-        dict = {
-            "occ_key" : occurenceKey,
-            "Latitude" : occurenceLat,
-            "Longitude" : occurenceLon,
-            "occurenceId": occurenceId
-        }
     
+    occurences = occurrences.search(taxonKey = specie.key, limit= limit, hasCoordinate=True, hasGeospatialIssue = False, decimalLongitude= decimal_longitude, decimalLatitude=decimal_latitude, country = 'CA')
 
-    occurences = pd.DataFrame.from_dict(dict)
-    return(occurences)
+
+    if occurences['count'] != 0:
+
+        # Message showing how many ocurences found, with limit input
+        print("Found {} available occurences for {} in Canada, limiting to {}".format((occurences['count']), specie.name, limit))
+
+        # Dataframe from occurences results
+        occ_df = pd.DataFrame.from_dict(occurences['results'])
+
+        # Cleaning dataframe with only relevant info from occurence query
+        occ_df = occ_df[['key', 
+                        'decimalLongitude', 
+                        'decimalLatitude', 
+                        'eventDate',
+                        'verbatimLocality',
+                        'stateProvince',
+                        'species',
+                        'occurrenceID']]
+
+        # Removing rows not in quebec
+
+        # Specified Canada only but Lat/ Long might make it in Ontario
+        occ_df = occ_df[occ_df.stateProvince == 'Québec']
+        occ_df = occ_df.reset_index(drop=True)
+
+
+        # Message printing how many occurences were in quebec
+        print('From {} checked observations {} were in Quebec and kept for analysis'.format(limit, len(occ_df)))
+        return(occ_df)
+    
+    else:
+        print('Found no occurences for {}'.format(specie.name))
+
+
+def downloadOccurences():
+    pass
 
 def writeOccurencesToFile(currentSpeciesFolder, overwrite = 1, limit = 2):
     if 'occ.csv' not in os.listdir(currentSpeciesFolder):
