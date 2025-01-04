45666b152e438e81246f5665394cdc69f02612c8
Etienne <manateetiti@gmail.com>
2025-01-04 18:07:12 -0500

    occ_df to occ_gdf, join with region_perimetre_gdf

---
+++ geodata.py
@@ -0,0 +1,51 @@
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+from occurences import create_occurences_dataframe
+
+def df_to_gdf(occ_df, xy = ['decimalLongitude', 'decimalLatitude']):
+
+    # Creates goepandas from dataframe with lat/long columns
+    occ_gdf = gpd.GeoDataFrame(
+        occ_df, geometry=gpd.points_from_xy(occ_df[xy[0]], occ_df[xy[1]]), crs="EPSG:4326"
+    )
+
+    return occ_gdf
+    
+def gpd_assign_region(occ_gdf):
+
+    regions_perimetre_file = 'data/geodata/regions_perimetre/regions_perimetre.shp'
+
+    # Creat gdf from regions perimetre
+    regions_gdf = gpd.read_file(regions_perimetre_file)
+
+    # Join two geodataframe (naturally skips points not within bounds of perimetre)
+    occ_gdf = gpd.sjoin(occ_gdf, regions_gdf, predicate='within')
+
+    # Remove unecessary collumns
+    occ_gdf = occ_gdf.drop(['index_right', 'decimalLongitude', 'decimalLatitude' ],axis=1)
+
+    # Rename region_code collumn
+    occ_gdf = occ_gdf.rename(columns={'idx_no_dec': 'region_code'})
+
+    return occ_gdf
+
+
+
+
+
+def populate_occurence_data(occ_gdf):
+
+    
+
+
+# loop
+
+    # load specific geo file based on region value
+
+    # compare lat long with geodata from foret ouverte 
+    # assign values of densite, cl_ag_et, tree_associations based on coordinate 
+
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from occurences import searchOccurences, get_download_zip, processOccurenceDownload
+from occurences import searchOccurences, get_download_zip, create_occurences_dataframe
 from specie import create_specie
+import geodata
 
 import pandas as pd
 import os
@@ -7,14 +8,21 @@ import os
 #Readable inaturalist links
 pd.set_option('display.max_colwidth', 1000)
 
+# Used to check if occurence file exists
 occurences_file = None
 
-specie_name = 'Cantharellus enelensis'
-specie = create_specie(specie_name, rank = 'Species')
+specie_query = 'Cantharellus enelensis'
 
-# Check if occurence download has already been made
+#Create specie object based on query name - Using gbif specie module
+specie = create_specie(specie_query, rank = 'Species')
+
+
+# xxxxxxxxxxxxxx Could be in one main function running from occurences.py and returning dataframe for rest xxxxxxxxxxxx
+
+# Check if occurence download has already been made for specific specie
 if 'download_key.txt' not in os.listdir('data/gbifQueries/{}'.format(specie.name)):
-    occ_df = searchOccurences(specie, download = True)
+    # Create occurence request
+    searchOccurences(specie, download = True)
 
 else:
     print('Occurence request already made for {}.'.format(specie.name))
@@ -22,11 +30,17 @@ else:
     print('')
     #Download request data to disk
     occurences_file = get_download_zip(specie)
-    #print(occ_df.head(50))
     print(occurences_file)
+    print('')
 
+# Create dataframe based on occurences
 if occurences_file != None:
-    processOccurenceDownload(occurences_file)
+    occ_df = create_occurences_dataframe(occurences_file)
+
+# xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 
+occ_gdf = geodata.df_to_gdf(occ_df)
+occ_gdf = geodata.gpd_assign_region(occ_gdf)
 
+print(occ_gdf.head())
