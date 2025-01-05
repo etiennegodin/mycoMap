0ef1140511abbbff75d25665ac2e856bc21f63e7
Etienne <manateetiti@gmail.com>
2025-01-04 23:58:21 -0500

    find closest point to occurence, merges geoc_maj value in gdf

--- geodata.py
+++ geodata.py
@@ -1,16 +1,19 @@
-import geopandas as gpd
 import matplotlib.pyplot as plt
+import os
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+from scipy.spatial import cKDTree
+from shapely.geometry import Point
 
-from occurences import create_occurences_dataframe
-
-def df_to_gdf(occ_df, xy = ['decimalLongitude', 'decimalLatitude']):
+def df_to_gdf(df, xy = ['decimalLongitude', 'decimalLatitude']):
 
     # Creates goepandas from dataframe with lat/long columns
-    occ_gdf = gpd.GeoDataFrame(
-        occ_df, geometry=gpd.points_from_xy(occ_df[xy[0]], occ_df[xy[1]]), crs="EPSG:4326"
+    gdf = gpd.GeoDataFrame(
+        df, geometry=gpd.points_from_xy(df[xy[0]], df[xy[1]]), crs="EPSG:4326"
     )
 
-    return occ_gdf
+    return gdf
     
 def gpd_assign_region(occ_gdf):
 
@@ -31,19 +34,91 @@ def gpd_assign_region(occ_gdf):
     return occ_gdf
 
 
+def ckdnearest(gdA, gdB):
 
+    nA = np.array(list(gdA.geometry.apply(lambda x: (x.x, x.y))))
+    nB = np.array(list(gdB.geometry.apply(lambda x: (x.x, x.y))))
+    btree = cKDTree(nB)
+    dist, idx = btree.query(nA, k=1)
+    gdB_nearest = gdB.iloc[idx].drop(columns="geometry").reset_index(drop=True)
 
+    gdf = pd.concat(
+        [
+            gdA.reset_index(drop=True),
+            gdB_nearest,
+            pd.Series(dist, name='dist'),
+        ], 
+        axis=1)
+
+    return gdf
 
 def populate_occurence_data(occ_gdf):
 
     regions_data_path = 'data/geodata/regions_data/'
-    region_gdf_path = regions_data_path + occ_gdf['region_code']
 
-    region_gdf = gpd.read_file(region_gdf_path)
+    print('')
+    print('Feeding {} occurences to populate'.format(len(occ_gdf)))
+    final_gdf = gpd.GeoDataFrame()
 
-    occ_gdf = gpd.sjoin(occ_gdf, region_gdf, predicate='within')
+    region_code_list = os.listdir(regions_data_path)
+    region_codes_amount = len(region_code_list)
+    #region_codes_index = region_codes_amount  1
+    for idx, region_code in enumerate(region_code_list):
 
-    print(occ_gdf)
+        # End loop once all folders are done iterating
+        if idx == region_codes_amount:
+            break
+        else:
+            print(region_code)
+
+            # Build path to read csv point of region
+            region_gdf_path = regions_data_path + region_code + '/' + '{}.csv'.format(region_code)
+
+            # Read region csv as dataframe
+            df = pd.read_csv(region_gdf_path)
+
+            # Reformat as gdf using xy as geometry
+            region_gdf = df_to_gdf(df, xy = ['X','Y'])
+
+            # Keep only relevant columns
+            region_gdf = region_gdf[['geometry', 'geoc_maj']]
+
+            # Keep only region occurences for region_occ_gdf
+            region_occ_gdf = occ_gdf[(occ_gdf['region_code']==region_code)]
+
+            # Check if occurence in this region
+            if len(region_occ_gdf) != 0:
+                print('{} occurences in {}'.format(len(region_occ_gdf), region_code))
+
+                # nearest 
+                gdf = ckdnearest(region_occ_gdf, region_gdf)
+                #assign value from csv matching with geoc_maj
+
+                #return partial df to final_gdf
+                final_gdf = pd.concat([final_gdf, gdf])
+
+            else:
+                print(' - 0 occurences in {} - '.format(region_code))
+                continue
+            
+    return final_gdf
+    
+
+
+
+
+    # Create geodataFrame for regions  
+    #region_gdf = gpd.read_file(region_gdf_path)
+    
+    #print(region_gdf)
+    # Read csv from region_code 
+
+    
+
+    #occ_gdf = gpd.sjoin(occ_gdf, region_gdf, predicate='within')
+
+    
+
 
 
 
